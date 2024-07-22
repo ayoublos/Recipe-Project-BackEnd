7bb6fd28e55464b4d784afb5a827537025b93e50
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-07-22 18:57:41 -0400

    changed port to 3333 so it does not interfere with frontend port. Formatted all js files. Changed country origin and category to match frontend

--- app.js
+++ app.js
@@ -14,13 +14,10 @@ const recipes = require("./controllers/recipeController.js");
 app.use("/recipes", recipes);
 
 // ROUTES
-app.get("/", (req, res) => {
-  res.send("Welcome to Recipes App");
-});
+app.get("/", (req, res) => res.send("Welcome to Recipes App"));
+
 // 404 PAGE
-app.get("*", (req, res) => {
-    res.status(404).send("Page not found");
-  });
+app.get("*", (req, res) => res.status(404).send("Page not found"));
 
 // EXPORT
 module.exports = app;
--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,7 +1,14 @@
 // controllers/colorsController.js
 const express = require("express");
 const recipes = express.Router();
-const { getRecipe,getAllRecipes, deleteRecipe, createRecipe,updateRecipe} = require("../queries/recipe");
+const {
+  getRecipe,
+  getAllRecipes,
+  deleteRecipe,
+  createRecipe,
+  updateRecipe,
+} = require("../queries/recipe");
+
 // const { checkName,checkBoolean } = require("../validations/checkColors.js");
 
 // INDEX
@@ -24,12 +31,14 @@ recipes.get("/:id", async (req, res) => {
     res.status(404).json({ error: "not found" });
   }
 });
+
 // CREATE
 recipes.post("/", async (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   const recipe = await createRecipe(req.body);
   res.json(recipe);
 });
+
 recipes.delete("/:id", async (req, res) => {
   const { id } = req.params;
   const deletedRecipe = await deleteRecipe(id);
@@ -39,8 +48,9 @@ recipes.delete("/:id", async (req, res) => {
     res.status(404).json("Color not found");
   }
 });
+
 recipes.put("/:id", async (req, res) => {
-    console.log(req.body)
+  console.log(req.body);
   const { id } = req.params;
   const updatedRecipe = await updateRecipe(id, req.body);
   res.status(200).json(updatedRecipe);
